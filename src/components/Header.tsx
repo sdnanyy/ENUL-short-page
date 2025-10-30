a9f3808245d7ba39552f7b1320658028e9d9b427
[dyad] <sdnanyy@gmail.com>
2025-10-30 19:15:42 -0300

    [dyad] Removendo import não utilizado de useState - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Dialog, Popover } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
