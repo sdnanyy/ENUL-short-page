8471688069a2b60749daea0c90f78d2617fe1c0a
[dyad] <sdnanyy@gmail.com>
2025-10-30 19:16:33 -0300

    Reverted all changes back to version 94dbb66de04d414e47b84e47113df2a3423967c4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Dialog, Popover } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
